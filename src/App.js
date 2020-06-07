a95ff5d2ff4ea13706387c614f296c1d1d4f0117
Linda Huang <sh837@cornell.edu>
2020-06-07 16:47:38 +0800

    cleaned up repo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Timer from './components/timer';
-import UrlTracker from './components/url'
+import Timer from './components/timer/timer';
 
 function App() {
   return (
     <div className="App">
       <Timer/>
-      <UrlTracker/>
     </div>
   );
 }
