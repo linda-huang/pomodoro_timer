c31647cf957dd2742d325ad823da8f63177976bf
Linda Huang <sh837@cornell.edu>
2020-06-28 11:31:55 +0800

    added a new final intermediate stage

--- src/Components/prompts/prompts.js
+++ src/Components/prompts/prompts.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { setPrevState, setCountdownState, WORK, BREAK, INTERMEDIATE } from '../timer/timerDucks';
+import { setPrevState, setCountdownState, WORK, BREAK, INTERMEDIATE, SESSION_END } from '../timer/timerDucks';
 import {connect} from 'react-redux';
 import Alert from '../audio/alert';
 import sadcat from './679796247967694906.png';
 import happycat from './568124064485343241.gif';
 
-function Prompts({countdown_state, prev_state, setPrevState, setCountdownState}) {
+function Prompts({countdown_state, prev_state, num_repeats, setPrevState, setCountdownState, setNumRepeats}) {
     const [prompt, setPrompt] = useState(null);
     const [img, setImg] = useState(null);
 
@@ -20,8 +20,14 @@ function Prompts({countdown_state, prev_state, setPrevState, setCountdownState})
             }
             setTimeout(() => {
                 setPrompt(null)
-                setCountdownState((prev_state === WORK) ? BREAK : WORK)
-                setPrevState(INTERMEDIATE)
+                if (countdown_state===SESSION_END) {
+                    setCountdownState(NONE)
+                    setPrevState(SESSION_END)
+                }
+                else {
+                    setCountdownState((prev_state === WORK) ? BREAK : WORK);
+                    setPrevState(INTERMEDIATE);
+                }
             }, 2000)
         }
     }, [countdown_state])
@@ -45,12 +51,14 @@ function Prompts({countdown_state, prev_state, setPrevState, setCountdownState})
 
 const mapStateToProps = state => ({
     countdown_state : state.countdown.countdown_state,
-    prev_state : state.countdown.prev_state
+    prev_state : state.countdown.prev_state,
+    num_repeats : state.settings.num_repeats
 })
 
 const mapDispatchToProps = dispatch => ({
     setCountdownState: state => dispatch(setCountdownState(state)),
-    setPrevState: state => dispatch(setPrevState(state))
+    setPrevState: state => dispatch(setPrevState(state)),
+    setNumRepeats: repeats => dispatch(setNumRepeats(repeats))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Prompts)
