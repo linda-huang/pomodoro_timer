971272d0161d9ef07eeeb2271840eca538704458
Doanh <ddt45@cornell.edu>
2020-06-30 18:38:47 -0700

    have no idea commiting before pulling color

--- src/Components/prompts/prompts.js
+++ src/Components/prompts/prompts.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { setPrevState, setCountdownState, WORK, BREAK, INTERMEDIATE, SESSION_END } from '../timer/timerDucks';
+import { setPrevState, setCountdownState, WORK, BREAK, INTERMEDIATE } from '../timer/timerDucks';
 import {connect} from 'react-redux';
 import Alert from '../audio/alert';
 import sadcat from './679796247967694906.png';
 import happycat from './568124064485343241.gif';
 
-function Prompts({countdown_state, prev_state, num_repeats, setPrevState, setCountdownState, setNumRepeats}) {
+function Prompts({countdown_state, prev_state, setPrevState, setCountdownState}) {
     const [prompt, setPrompt] = useState(null);
     const [img, setImg] = useState(null);
 
@@ -20,14 +20,8 @@ function Prompts({countdown_state, prev_state, num_repeats, setPrevState, setCou
             }
             setTimeout(() => {
                 setPrompt(null)
-                if (countdown_state===SESSION_END) {
-                    setCountdownState(NONE)
-                    setPrevState(SESSION_END)
-                }
-                else {
-                    setCountdownState((prev_state === WORK) ? BREAK : WORK);
-                    setPrevState(INTERMEDIATE);
-                }
+                setCountdownState((prev_state === WORK) ? BREAK : WORK)
+                setPrevState(INTERMEDIATE)
             }, 2000)
         }
     }, [countdown_state])
@@ -37,12 +31,15 @@ function Prompts({countdown_state, prev_state, num_repeats, setPrevState, setCou
     else {
         return(
             <div className = "intermission">
+                
                 <h2 className = "prompt">{prompt}</h2>
 
                 <div className = "cat">
-                <img src = {img} alt = "cat"/>
+                    <img src = {img} alt = "cat"/>
                 </div>
+
                 <Alert/>
+
             </div>
         )
     
@@ -51,14 +48,12 @@ function Prompts({countdown_state, prev_state, num_repeats, setPrevState, setCou
 
 const mapStateToProps = state => ({
     countdown_state : state.countdown.countdown_state,
-    prev_state : state.countdown.prev_state,
-    num_repeats : state.settings.num_repeats
+    prev_state : state.countdown.prev_state
 })
 
 const mapDispatchToProps = dispatch => ({
     setCountdownState: state => dispatch(setCountdownState(state)),
-    setPrevState: state => dispatch(setPrevState(state)),
-    setNumRepeats: repeats => dispatch(setNumRepeats(repeats))
+    setPrevState: state => dispatch(setPrevState(state))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Prompts)
